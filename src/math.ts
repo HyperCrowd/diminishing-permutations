37a70193fb67291a966b0db04724201957578ae2
HyperCrowd <76200774+HyperCrowd@users.noreply.github.com>
2023-04-24 16:49:46 -0400

    4 files modified

--- src/math.ts
+++ src/math.ts
@@ -12,36 +12,12 @@ function stirlingApproximation(n: BigNumber) {
   if (n.isEqualTo(1)) {
     return ONE;
   }
-  console.log('left');
+
   const left = n.times(PI).times(TWO).sqrt();
-  console.log('right');
-  const right = n.dividedBy(E);
-  console.log('product');
-  const product = left.times(power(right, n));
+  const right = n.dividedBy(E).pow(n);
+  const product = left.times(right);
   console.log(product);
   return product;
-}
-
-/**
- *
- */
-function power(base: BigNumber, exponent: BigNumber): BigNumber {
-  let result = new BigNumber(1);
-  let multiplier = base;
-  let remainder = exponent;
-
-  while (remainder.gt(0)) {
-    console.log(remainder.gt(0));
-    if (remainder.modulo(2).eq(1)) {
-      result = result.times(multiplier);
-    }
-    console.log('mult');
-    multiplier = multiplier.times(multiplier);
-    console.log('div');
-    remainder = remainder.dividedBy(2);
-  }
-  console.log('done');
-  return result;
 }
 
 /**
@@ -70,21 +46,13 @@ export function getPermutations(n: number) {
   const N = new BigNumber(n);
   let factor;
 
-  if (n > 10000) {
-    console.log('stir', n);
-    factor = {
-      value: stirlingApproximation(N),
-    };
-  } else {
-    console.log('iterate', n);
-    const factorialGenerator = factorial(N);
-    factor = factorialGenerator.next();
+  const factorialGenerator = factorial(N);
+  factor = factorialGenerator.next();
 
-    while (!factor.done) {
-      factor = factorialGenerator.next();
-    }
+  while (!factor.done) {
+    factor = factorialGenerator.next();
   }
-  console.log('here', n);
+
   const eTimesNFactorial = E.times(factor.value);
   const result = floor(eTimesNFactorial).minus(1);
 
